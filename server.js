cdae1cbfe220b171028d8c92f77543958284ffdf
Hoang Le <lehuyhoang117@gmail.com>
2018-03-22 22:40:13 +0700

    file upload

--- server.js
+++ server.js
@@ -2,29 +2,32 @@ const express = require('express')
 const fileUpload = require('express-fileupload')
 var morgan = require('morgan')
 var errorHandler = require('errorhandler')
+var path = require('path')
 var { green } = require('chalk')
 
 const app = express()
+
+var uploadDir = path.join(__dirname, '/upload')
 
 var port = process.env.PORT || 3000
 app.set('port', port)
 app.use(morgan('dev'))
 app.use(errorHandler())
-
-// default options
 app.use(fileUpload())
+app.use(express.static(uploadDir))
 
 app.post('/upload', function(req, res) {
   if (!req.files)
     return res.status(400).send('No files were uploaded.')
 
   let sampleFile = req.files.sampleFile
+  let fileName = req.files.sampleFile.name
 
-  sampleFile.mv(__dirname + '/upload/'+req.files.sampleFile.name, function(err) {
+  sampleFile.mv(path.join(uploadDir, fileName), function(err) {
     if (err)
       return res.status(500).send(err)
 
-    res.send('File uploaded!')
+    res.send(`File uploaded!<p/><a target="_blank" href="/${fileName}">${fileName}</a>`)
   })
 })
 
